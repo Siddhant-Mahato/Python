83dbdc982518a81c270526cd094efe9f6149f690
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2024-01-30 05:53:39 +0530

    First Exercise Based On Concept's Learned By Far

---
+++ 007-Exercise-1-Create-a-Calculator/main.py
@@ -0,0 +1,18 @@
+print(5+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6)
+print(5%3)
+print(2**4)
+
+a = 8
+b = 9
+c = a + b
+print("Answer In Addition Way = ",c)
+c = a - b
+print("Answer In Subtraction Way = ",c)
+c = a * b
+print("Answer In Multiplicatoion Way = ",c)
+c = a / b
+print("Answer In Division Way = ",c)
