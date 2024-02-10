843d5eb9c911418babf45a82ee64d18311380e72
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2024-02-10 12:50:33 +0530

    About If-else In Python

---
+++ 014-If-Else-Conditionals/05 practice.py
@@ -0,0 +1,13 @@
+a = 8
+b = 9
+
+if((a + b) < 10):
+    if((a+b) & 1 == 0):
+        print("Even")
+    
+    else:
+        print("Odd")
+elif ((a + b) > 13):
+    print("Hello")
+else:
+    print("Done")
